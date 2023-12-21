9cd61b45137ee1ffc4ed009131c64c20c213dbc4
artimkaf <artimkaf228@gmail.com>
2023-12-21 13:43:51 +0300

    all 2 floors done (predzashita ver.)

--- Assets/Scripts/char_sprite.cs
+++ Assets/Scripts/char_sprite.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class char_sprite : MonoBehaviour
 {
-    public RuntimeAnimatorController animator_0;
-    public RuntimeAnimatorController animator_1;
+    public RuntimeAnimatorController animator_0; //ссылка на пак с анимациями женского персонажа
+    public RuntimeAnimatorController animator_1; //ссылка на пак с анимациями мужского персонажа
 
     void Start()
     {
-        if (PlayerPrefs.GetInt("player_id") == 1)
+        if (PlayerPrefs.GetInt("player_id") == 1) // если был выбран персонаж мужчина
         {
-            GetComponent<Animator>().runtimeAnimatorController = animator_1;
+            GetComponent<Animator>().runtimeAnimatorController = animator_1; //пак анимаций персонажа смениться на пак с анимацииями мужского персонажа
         }
         else
         {
-            GetComponent<Animator>().runtimeAnimatorController = animator_0;
+            GetComponent<Animator>().runtimeAnimatorController = animator_0; //пак анимаций персонажа смениться на пак с анимацииями женского персонажа
         }
     }
 }
--- Assets/Scripts/move_script.cs
+++ Assets/Scripts/move_script.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class movePlayer : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rb;
+    public float speed; // скорость персонажа
+    private Rigidbody2D rb; //переменная Rigidbody2D
 
-    public Animator animator;
-    private Vector2 direction;
+    public Animator animator; //ссылка на аниматор персонажа
+    private Vector2 direction; //перменная с напрвлением персонажа
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //ссылка на компонент Rigidbody2D
     }
 
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = Input.GetAxis("Vertical");
+        direction.x = Input.GetAxis("Horizontal"); // координаты персонажа по оси X
+        direction.y = Input.GetAxis("Vertical"); // координаты персонажа по оси Y
         
-        animator.SetFloat("Horizontal", direction.x);
-        animator.SetFloat("Vertical", direction.y);
-        animator.SetFloat("speed", direction.sqrMagnitude);
+        animator.SetFloat("Horizontal", direction.x); // координаты анимации персонажа по оси X
+        animator.SetFloat("Vertical", direction.y); // координаты анимации персонажа по оси Y
+        animator.SetFloat("speed", direction.sqrMagnitude); // скорость анимации персонажа
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime); // функция которая передвигает персонажа
     }
 }
